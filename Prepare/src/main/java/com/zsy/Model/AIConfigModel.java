145e397ad5523db8fc0644d169868b1a73499964
3timelike <2424467624@qq.com>
2025-06-23 02:31:59 +0800

    fix:updateAIModel

--- Prepare/src/main/java/com/zsy/Model/AIConfigModel.java
+++ Prepare/src/main/java/com/zsy/Model/AIConfigModel.java
@@ -9,7 +9,14 @@ import lombok.Setter;
 @Getter
 public class AIConfigModel {
      String DefaultSystemMessage = "你是一个Java高手，熟读各种源码";
-     String configFile = "import java.util.*;\n" +
+     String configFile = "import org.springframework.ai.chat.client.ChatClient;\n" +
+             "import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;\n" +
+             "import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;\n" +
+             "import org.springframework.ai.chat.memory.ChatMemory;\n" +
+             "import org.springframework.ai.chat.memory.InMemoryChatMemory;\n" +
+             "import org.springframework.ai.ollama.OllamaChatModel;\n" +
+             "import org.springframework.context.annotation.Bean;\n" +
+             "import org.springframework.context.annotation.Configuration;\n" +
              "\n" +
              "@Configuration\n" +
              "public class ChatClientConfig {\n" +
@@ -23,14 +30,14 @@ public class AIConfigModel {
              "    public ChatClient chatClient(OllamaChatModel model, ChatMemory chatMemory) {\n" +
              "        return ChatClient\n" +
              "                .builder(model)\n" +
-             "                .defaultSystem(\"" + DefaultSystemMessage + "\")\n" +
+             "                .defaultSystem(\"" + DefaultSystemMessage +"\")\n" +
              "                .defaultAdvisors(\n" +
              "                        new SimpleLoggerAdvisor(),\n" +
              "                        new MessageChatMemoryAdvisor(chatMemory)\n" +
              "                )\n" +
              "                .build();\n" +
              "    }\n" +
-             "}\n";
+             "}";
 
 
 }
--- Prepare/src/main/java/com/zsy/Model/AIControlModel.java
+++ Prepare/src/main/java/com/zsy/Model/AIControlModel.java
@@ -6,7 +6,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AIControlModel {
-      String file = "@RequiredArgsConstructor\n" +
+      String file = "import lombok.RequiredArgsConstructor;\n" +
+              "import org.springframework.ai.chat.client.ChatClient;\n" +
+              "import org.springframework.web.bind.annotation.RequestMapping;\n" +
+              "import org.springframework.web.bind.annotation.RequestParam;\n" +
+              "import org.springframework.web.bind.annotation.RestController;\n" +
+              "import reactor.core.publisher.Flux;\n" +
+              "\n" +
+              "import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;\n" +
+              "\n" +
+              "@RequiredArgsConstructor\n" +
               "@RestController\n" +
               "@RequestMapping(\"/ai\")\n" +
               "public class ChatController {\n" +
